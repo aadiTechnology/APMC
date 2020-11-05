bbf4d2e556600c709157c8c24332664876c6d562
unknown <ganesh.rj38@yahoo.com>
2020-11-05 14:58:05 +0530

    Stall registration, In main layout <router-outlet > div change

--- Code/APMCUI/src/app/merchant/merchant-routing.module.ts
+++ Code/APMCUI/src/app/merchant/merchant-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StallRegistrationComponent } from './components/merchant-dashboard/stall-registration/stall-registration.component';
 
 import { MerchantComponent } from './merchant.component';
 
-const routes: Routes = [{ path: '', component: MerchantComponent }];
+const routes: Routes = [{ path: '', component: MerchantComponent,
+children: [
+  { path: '**', redirectTo: 'stallregistration', pathMatch: 'full' },
+  { path: 'stallregistration', component: StallRegistrationComponent }
+]
+
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Code/APMCUI/src/app/merchant/merchant.module.ts
+++ Code/APMCUI/src/app/merchant/merchant.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MerchantRoutingModule } from './merchant-routing.module';
 import { MerchantComponent } from './merchant.component';
 import { MerchantDashboardComponent } from './components/merchant-dashboard/merchant-dashboard.component';
+import { StallRegistrationComponent } from './components/merchant-dashboard/stall-registration/stall-registration.component';
 
 
 @NgModule({
-  declarations: [MerchantComponent, MerchantDashboardComponent],
+  declarations: [MerchantComponent, MerchantDashboardComponent, StallRegistrationComponent],
   imports: [
     CommonModule,
     MerchantRoutingModule
