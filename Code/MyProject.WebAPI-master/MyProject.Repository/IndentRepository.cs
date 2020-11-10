ff0c5797e40761b9867561c02dc149e1ed5d51ab
aadiTechnology <aaditechology@gmail.com>
2020-11-10 10:52:30 +0530

    Indent creation API code merge

---
+++ Code/MyProject.WebAPI-master/MyProject.Repository/IndentRepository.cs
@@ -0,0 +1,48 @@
+﻿using MyProject.Contracts;
+using MyProject.Entities;
+using MyProject.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject.Repository
+{
+    public class IndentRepository : RepositoryBase<IndentDetails>, IIndentRepository
+    {
+        private readonly RepositoryContext _repositoryContext;
+        public IndentRepository(RepositoryContext repositoryContext)
+           : base(repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+        public IndentDetails Add(IndentDetails indentDetails, List<IndentProducts> indentProducts)
+        {
+            try
+            {
+                _repositoryContext.IndentDetails.Add(indentDetails);
+                _repositoryContext.IndentProducts.AddRange(indentProducts);
+                _repositoryContext.SaveChanges();
+                return indentDetails;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        public IndentDetails Update(IndentDetails indentDetails)
+        {
+            try
+            {
+                _repositoryContext.IndentDetails.Update(indentDetails);
+                _repositoryContext.SaveChanges();
+                return indentDetails;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
