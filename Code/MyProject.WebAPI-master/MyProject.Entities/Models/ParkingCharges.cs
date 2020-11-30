8d30aa796dbdc367e689fa6354e1a0964c631d6b
aadiTechnology <aaditechology@gmail.com>
2020-11-30 20:44:54 +0530

    Exit Gate operator functionality added.

--- Code/MyProject.WebAPI-master/MyProject.Entities/Models/ParkingCharges.cs
+++ Code/MyProject.WebAPI-master/MyProject.Entities/Models/ParkingCharges.cs
@@ -11,9 +11,14 @@ namespace MyProject.Entities.Models
         public string VehicleTypeId { get; set; }
         public string VehicleNumber { get; set; }
         public string EntryFee { get; set; }
-        public int ChargesTypeId { get; set; }
         public DateTime? InTime { get; set; }
         public DateTime? OutTime { get; set; }
-       
+        public string NoParkingFee { get; set; }
+        public string ExtraTimeFee { get; set; }
+        public int ExtraTime { get; set; }
+        public string FineCharges { get; set; }
+        public int UpdatedById { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
     }
 }
--- Code/MyProject.WebAPI-master/MyProject/Controllers/ExitGateController.cs
+++ Code/MyProject.WebAPI-master/MyProject/Controllers/ExitGateController.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyProject.Contracts;
 using MyProject.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MyProject.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "ExitGateOperator")]
+
     public class ExitGateController : ControllerBase
     {
         public override void InitializeController() { }
@@ -18,11 +19,46 @@ namespace MyProject.WebAPI.Controllers
         {
             RepositoryWrapper = repositoryWrapper;
         }
+
         public IRepositoryWrapper RepositoryWrapper { get; }
+        /// <summary>
+        /// This method is used to get all checkin vehical details of current date.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("GetAllCheckInVehicalDetails")]
         public async Task<JsonResult> GetAllCheckInVehicalDetails()
         {
             return await base.FinalizeMultiple<IEnumerable<ParkingCharges>>(await RepositoryWrapper.ExitGateRepository.GetAllCheckInVehicalDetails());
+        }
+
+        /// <summary>
+        /// This method is used to get checkin details of selected vehical.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpGet("GetCheckInVehicalDetailsById")]
+        public async Task<JsonResult> GetCheckInVehicalDetailsById(int Id)
+        {
+            return await base.FinalizeMultiple<IEnumerable<ParkingCharges>>(await RepositoryWrapper.ExitGateRepository.GetCheckInVehicalDetailsById(Id));
+        }
+
+        /// <summary>
+        /// This method is used to update exit details of selected vehical.
+        /// </summary>
+        /// <param name="parkingCharges"></param>
+        /// <returns></returns>
+        [HttpPost("UpdateParkingCharges")]
+        public async Task<JsonResult> UpdateParkingCharges([FromBody] ParkingCharges parkingCharges)
+        {
+            try
+            {
+                RepositoryWrapper.ExitGateRepository.UpdateParkingCharges(parkingCharges);
+                return await base.FinalizeMessage("Vehical exit succesfully");
+            }
+            catch (Exception ex)
+            {
+                return await base.FinalizStatusCodeeMessage("Error: Failure in ParkingCharges Update : " + ex, 401);
+            }
         }
     }
 }
