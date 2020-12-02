5e7378c7a9c93df7b2d54d5aeec53730a26a7b35
Sangramskonde <71622656+Sangramskonde@users.noreply.github.com>
2020-12-02 12:51:39 +0530

    Entry Gate Indent List API integrated

--- Code/APMCUI/src/app/entry-gate-operator/components/entry-gate-operator.service.ts
+++ Code/APMCUI/src/app/entry-gate-operator/components/entry-gate-operator.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpService } from "../../core/services/http.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EntryGateOperatorService {
-
-  constructor() { }
+  constructor(private httpService: HttpService) {}
+  getAllIndentDetails(Id): any {
+    return this.httpService.get(`EntryGate/IndentDetailsById?Id=${Id}`);
+  }
+  getAllNotScannedIndent(): any {
+    return this.httpService.get("EntryGate/GetAllNotScannedIndent");
+  }
 }
--- Code/APMCUI/src/app/entry-gate-operator/components/indent-list-record/indent-list-record.component.ts
+++ Code/APMCUI/src/app/entry-gate-operator/components/indent-list-record/indent-list-record.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { from } from "rxjs";
+import { EntryGateOperatorService } from "../entry-gate-operator.service";
+import{IndentList} from "../../entities/indentDetails";
 
 @Component({
-  selector: 'app-indent-list-record',
-  templateUrl: './indent-list-record.component.html',
-  styleUrls: ['./indent-list-record.component.scss']
+  selector: "app-indent-list-record",
+  templateUrl: "./indent-list-record.component.html",
+  styleUrls: ["./indent-list-record.component.scss"],
 })
 export class IndentListRecordComponent implements OnInit {
-  myDate = Date.now();    //date 
+  myDate = Date.now(); //date
+  IndentList: IndentList[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private entryGateOperatorService: EntryGateOperatorService) {
+    this.IndentList= new Array<IndentList>();
   }
 
+  ngOnInit(): void {
+    this.getAllNotScannedIndent();
+    //this.getAllIndentDetails(Id);
+  }
+  getAllNotScannedIndent() {
+    this.entryGateOperatorService.getAllNotScannedIndent().subscribe((arg) => {
+      if (arg) {
+      this.IndentList=arg.rows;
+      alert(JSON.stringify(this.IndentList));
+      }
+    });
+  }
+  // getAllIndentDetails(Id){
+  //   this.entryGateOperatorService.getAllIndentDetails(Id).subscribe((arg) => {
+  //     if (arg) {
+  //     }
+  //   });
+  // }
 }
