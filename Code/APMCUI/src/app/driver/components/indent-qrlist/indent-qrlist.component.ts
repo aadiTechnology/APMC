a2fa03bfd47dffe8f783704d632c5089627f8a61
Sangramskonde <71622656+Sangramskonde@users.noreply.github.com>
2020-12-07 11:02:54 +0530

    Create Indent Page Changes

--- Code/APMCUI/src/app/driver/components/indent-qrlist/indent-qrlist.component.ts
+++ Code/APMCUI/src/app/driver/components/indent-qrlist/indent-qrlist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DriverService } from "../../driver.service";
+import { IndentDetails } from "../../entities/indentDetails";
+
+
 
 @Component({
   selector: 'app-indent-qrlist',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./indent-qrlist.component.scss']
 })
 export class IndentQRListComponent implements OnInit {
-  myDate = Date.now();    //date 
-  constructor() { }
+  myDate = Date.now();  
+  indentDetailList:any;
+  indentDetails:any;  //date 
+  constructor(
+    private driverService:DriverService,
+
+  ) {
+    this.indentDetails=new Array<IndentDetails>();
+   }
 
   ngOnInit(): void {
   }
+ 
+  getIndentWithMerchantName(){
+    this.driverService.getIndentWithMerchantName().subscribe((arg)=>{
+      if(arg){
+        this.indentDetailList=arg.rows[0];
+       alert(JSON.stringify(this.indentDetailList));
 
+      }
+    })
+  }
 }
